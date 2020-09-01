74545a481c67a4f7b6ac1cab1c1c8bf45edca975
mangku17 <sparky17@naver.com>
2020-09-01 15:04:03 +0900

    20200901_15pm 미정
    파이어베이스연동, IOT 페이지 추가

---
+++ WithPet/app/src/main/java/com/example/withpet/Menu5frag.java
@@ -0,0 +1,24 @@
+package com.example.withpet;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+//Mypage page
+public class Menu5frag extends Fragment {
+
+    private View rootview;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        rootview = inflater.inflate(R.layout.activity_iot,container,false);
+        // test주석입니다.
+        return rootview;
+    }
+}
