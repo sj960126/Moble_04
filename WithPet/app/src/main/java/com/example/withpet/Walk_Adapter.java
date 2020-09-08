0968323c6dee757b2be07a936706cb0b8ed76bd4
kwonyongjun <dydwns18011@gmail.com>
2020-09-08 11:55:46 +0900

    권용준_산책_게시판_업로드

--- WithPet/app/src/main/java/com/example/withpet/Walk_Adapter.java
+++ WithPet/app/src/main/java/com/example/withpet/Walk_Adapter.java
@@ -1,6 +1,7 @@
 package com.example.withpet;
 
 import android.content.Context;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -19,11 +20,11 @@ import java.util.List;
 
 public class Walk_Adapter extends RecyclerView.Adapter<Walk_Adapter.CustomViewholder>{
 
-    private  ArrayList<Walk_news> arrayList;
+    private  ArrayList<Walk_boardUpload> arrayList;
     private Context context;
 
 
-    public Walk_Adapter(ArrayList<Walk_news> arrayList,Context context){
+    public Walk_Adapter(ArrayList<Walk_boardUpload> arrayList,Context context){
         this.arrayList =arrayList;
         this.context = context;
 
@@ -38,11 +39,14 @@ public class Walk_Adapter extends RecyclerView.Adapter<Walk_Adapter.CustomViewho
 
     @Override
     public void onBindViewHolder(@NonNull CustomViewholder holder, int position) {
-        Glide.with(holder.itemView)
+        /*Glide.with(holder.itemView)
                 .load(arrayList.get(position).getProfile())
-                .into(holder.iv_profile);
-        holder.tv_title.setText(arrayList.get(position).getTitle());
-        holder.tv_contents.setText(arrayList.get(position).getContext());
+                .into(holder.iv_profile);*/
+        Log.i("adapter:", arrayList.get(position).getWalkboard_title());
+        Log.i("adapter:", arrayList.get(position).getWalkboard_content());
+
+        holder.tv_title.setText(arrayList.get(position).getWalkboard_title());
+        holder.tv_contents.setText(arrayList.get(position).getWalkboard_content());
 
     }
 
@@ -52,15 +56,13 @@ public class Walk_Adapter extends RecyclerView.Adapter<Walk_Adapter.CustomViewho
     }
 
     public class CustomViewholder extends RecyclerView.ViewHolder {
-        ImageView iv_profile;
+      //  ImageView iv_profile;
         TextView tv_title;
         TextView tv_contents;
 
-
-
         public CustomViewholder(@NonNull View itemView) {
             super(itemView);
-            this.iv_profile = itemView.findViewById(R.id.imageView);
+        //    this.iv_profile = itemView.findViewById(R.id.imageView);
             this.tv_title = itemView.findViewById(R.id.walk_title);
             this.tv_contents = itemView.findViewById(R.id.walk_content);
 
@@ -68,10 +70,10 @@ public class Walk_Adapter extends RecyclerView.Adapter<Walk_Adapter.CustomViewho
                 @Override
                 public void onClick(View view) {
                     int pos = getAdapterPosition();
-                    Toast.makeText(context, "제목 : " + arrayList.get(pos).getTitle() + "내용 : " + arrayList.get(pos).getContext(), Toast.LENGTH_SHORT).show();
+                    Toast.makeText(context, "제목 : " + arrayList.get(pos).getWalkboard_title() + "내용 : " + arrayList.get(pos).getWalkboard_content(), Toast.LENGTH_SHORT).show();
 
                     if (pos != RecyclerView.NO_POSITION){
-                        Toast.makeText(context, "hihi"+pos, Toast.LENGTH_SHORT).show();
+
                     }
                 }
             });
