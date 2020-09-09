9d08fb7425692e5d5dad017a8792fe3d9fac34bd
mangku17 <sparky17@naver.com>
2020-09-09 15:05:58 +0900

    0909_15pm 미정
    회원가입, 게시글이름

--- withpet/app/src/main/java/com/withpet/main/HomeFrag.java
+++ withpet/app/src/main/java/com/withpet/main/HomeFrag.java
@@ -1,8 +1,10 @@
 package com.withpet.main;
 
+
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,6 +13,7 @@ import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -20,35 +23,45 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+
 import com.withpet.iot.*;
 import com.withpet.mypage.*;
 import com.withpet.newsfeed.*;
 import com.withpet.health.*;
-import com.withpet.walk.*;
 import com.withpet.*;
+
+
 
 import java.util.ArrayList;
 
-//Mypage page
-public class mypage_frag extends Fragment {
-
+//NewsFeed page
+public class HomeFrag extends Fragment {
     private View rootview;
     private RecyclerView list;
     private RecyclerView.Adapter adapter;
     private RecyclerView.LayoutManager layoutManager;
     private ArrayList<News> myfeed;
+
     private FirebaseDatabase db;
     private DatabaseReference dbreference;
-    final  int requestcode = 1001;
+
+    private Button btn_wirte;
+
+    private String[] permission_list = {Manifest.permission.READ_EXTERNAL_STORAGE};
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        rootview = inflater.inflate(R.layout.activity_mypage,container,false);
-        Button btn1 = (Button) rootview.findViewById(R.id.mypageBtn_setting);
-        btn1.setBackgroundResource(R.drawable.iconsetting);
+        rootview = inflater.inflate(R.layout.activity_news,container,false);
+        btn_wirte = rootview.findViewById(R.id.walkBtn_write);
 
-        list = rootview.findViewById(R.id.myPageListview_mynotice);
+        Button btn1 = (Button) rootview.findViewById(R.id.walkBtn_write);
+        Button btn2 = (Button) rootview.findViewById(R.id.mainBtn_chatt);
+
+        btn1.setBackgroundResource(R.drawable.iconadd);
+        btn2.setBackgroundResource(R.drawable.iconchatt);
+
+        list = rootview.findViewById(R.id.mainRv);
         list.setHasFixedSize(true); //리사이클러뷰 기존 성능 강화
         layoutManager = new LinearLayoutManager(getActivity());
         list.setLayoutManager(layoutManager);
@@ -56,6 +69,8 @@ public class mypage_frag extends Fragment {
 
         db = FirebaseDatabase.getInstance(); //파이어베스 데이터베이스 연동
         dbreference = db.getReference("Feed");//연동한 DB의 테이블 연결
+
+        //실시간으로 앱데이터를 업데이트 함수
         dbreference.addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
@@ -64,9 +79,6 @@ public class mypage_frag extends Fragment {
                 for(DataSnapshot snapshot: dataSnapshot.getChildren()){
                     //반복문으로 데이터 리스트를 추출
                     News news = snapshot.getValue(News.class); //만들어뒀던 news 객체에 데이터를 담음
-
-                    //Log.i("tIIIIIDDDDDDD ::", ""+news.imgUrl);
-                    //Log.i("tqtqtqtqtqtq ::", ""+news.id);
                     myfeed.add(news); //담은 데이터들을 배열리스터에 넣고 리사이클뷰로 보낼 준비
                 }
                 adapter.notifyDataSetChanged(); //리스트 저장 및 새로고침
@@ -78,24 +90,64 @@ public class mypage_frag extends Fragment {
                 Toast.makeText(getActivity(), "에러라라고오오옹", Toast.LENGTH_SHORT).show();
             }
         });
-        adapter = new MyPageNoticeAdapter(myfeed, getContext());
+        adapter = new MyFeedAdapter(myfeed, getContext());
         list.setAdapter(adapter); //리사이클러뷰에 어댑터 연결
-        rootview.findViewById(R.id.myPageBtn_modify).setOnClickListener(new View.OnClickListener() {
+
+        btn_wirte.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent modify = new Intent(getActivity(), ProfileModifyActivity.class);
-                startActivityForResult(modify, requestcode);
+                Toast.makeText(getActivity(), "클릭햇어......", Toast.LENGTH_SHORT).show();
+                //권한설정 함수 실행
+                Permission();
             }
         });
         return rootview;
     }
+
+    //권한설정 _ 안드로이드스튜디오개발가이드 + 구글링
+    public void Permission(){
+        for(String permission : permission_list){
+            //권한 확인 여부
+            int access = ContextCompat.checkSelfPermission(getContext(), permission);
+            //권한 x
+            if(access == PackageManager.PERMISSION_DENIED){
+                //권한 설정 확인 창이 뜸!!
+                //requestCode는 아래의 onRequestPermissionsResult 함수 실행의 매개변수!
+                requestPermissions(permission_list,0);
+            }
+            //권한 o
+            else{
+                //해당 페이지로 이동
+                Intent intent = new Intent(getContext(), NewsGalleryActivity.class);
+                intent.putExtra("request", R.integer.newsRequestcode);
+                startActivity(intent);
+            }
+        }
+    }
+
+    //권한 요청 후 어떻게 관리할 것인지에 대한 함수 _ 개발가이드에 자세히 나와있음
     @Override
-    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-        if(resultCode == getActivity().RESULT_OK){
-            ProfileInfo pinfo = (ProfileInfo) data.getSerializableExtra("profileinfo");
-            Log.i("username : ",pinfo.getUsername());
-            Log.i("shape : ",pinfo.getShape());
-            Log.i("meal : ",""+pinfo.getMeal());
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if(requestCode == 0)
+        {
+            for(int i=0; i<grantResults.length; i++)
+            {
+                //사용자가 허용했을 경우!
+                if(grantResults[i]==PackageManager.PERMISSION_GRANTED){
+                    //페이지 이동
+                    Intent intent = new Intent(getContext(), NewsGalleryActivity.class);
+                    startActivity(intent);
+                }
+                //사용자가 거부했을 경우!
+                else {
+                    Toast.makeText(getContext(),"저장소 권한을 설정하세요.",Toast.LENGTH_LONG).show();
+                }
+            }
         }
     }
 }
+
+
+
+
--- withpet/app/src/main/java/com/withpet/main/MypageFrag.java
+++ withpet/app/src/main/java/com/withpet/main/MypageFrag.java
@@ -1,10 +1,8 @@
 package com.withpet.main;
 
-
-import android.Manifest;
 import android.content.Intent;
-import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,7 +11,6 @@ import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -23,45 +20,35 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
-
 import com.withpet.iot.*;
 import com.withpet.mypage.*;
 import com.withpet.newsfeed.*;
 import com.withpet.health.*;
+import com.withpet.walk.*;
 import com.withpet.*;
-
-
 
 import java.util.ArrayList;
 
-//NewsFeed page
-public class home_frag extends Fragment {
+//Mypage page
+public class MypageFrag extends Fragment {
+
     private View rootview;
     private RecyclerView list;
     private RecyclerView.Adapter adapter;
     private RecyclerView.LayoutManager layoutManager;
     private ArrayList<News> myfeed;
-
     private FirebaseDatabase db;
     private DatabaseReference dbreference;
-
-    private Button btn_wirte;
-
-    private String[] permission_list = {Manifest.permission.READ_EXTERNAL_STORAGE};
+    final  int requestcode = 1001;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        rootview = inflater.inflate(R.layout.activity_news,container,false);
-        btn_wirte = rootview.findViewById(R.id.walkBtn_write);
+        rootview = inflater.inflate(R.layout.activity_mypage,container,false);
+        Button btn1 = (Button) rootview.findViewById(R.id.mypageBtn_setting);
+        btn1.setBackgroundResource(R.drawable.iconsetting);
 
-        Button btn1 = (Button) rootview.findViewById(R.id.walkBtn_write);
-        Button btn2 = (Button) rootview.findViewById(R.id.mainBtn_chatt);
-
-        btn1.setBackgroundResource(R.drawable.iconadd);
-        btn2.setBackgroundResource(R.drawable.iconchatt);
-
-        list = rootview.findViewById(R.id.mainRv);
+        list = rootview.findViewById(R.id.myPageListview_mynotice);
         list.setHasFixedSize(true); //리사이클러뷰 기존 성능 강화
         layoutManager = new LinearLayoutManager(getActivity());
         list.setLayoutManager(layoutManager);
@@ -69,8 +56,6 @@ public class home_frag extends Fragment {
 
         db = FirebaseDatabase.getInstance(); //파이어베스 데이터베이스 연동
         dbreference = db.getReference("Feed");//연동한 DB의 테이블 연결
-
-        //실시간으로 앱데이터를 업데이트 함수
         dbreference.addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
@@ -79,6 +64,9 @@ public class home_frag extends Fragment {
                 for(DataSnapshot snapshot: dataSnapshot.getChildren()){
                     //반복문으로 데이터 리스트를 추출
                     News news = snapshot.getValue(News.class); //만들어뒀던 news 객체에 데이터를 담음
+
+                    //Log.i("tIIIIIDDDDDDD ::", ""+news.imgUrl);
+                    //Log.i("tqtqtqtqtqtq ::", ""+news.id);
                     myfeed.add(news); //담은 데이터들을 배열리스터에 넣고 리사이클뷰로 보낼 준비
                 }
                 adapter.notifyDataSetChanged(); //리스트 저장 및 새로고침
@@ -90,64 +78,24 @@ public class home_frag extends Fragment {
                 Toast.makeText(getActivity(), "에러라라고오오옹", Toast.LENGTH_SHORT).show();
             }
         });
-        adapter = new MyFeedAdapter(myfeed, getContext());
+        adapter = new MyPageNoticeAdapter(myfeed, getContext());
         list.setAdapter(adapter); //리사이클러뷰에 어댑터 연결
-
-        btn_wirte.setOnClickListener(new View.OnClickListener() {
+        rootview.findViewById(R.id.myPageBtn_modify).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(getActivity(), "클릭햇어......", Toast.LENGTH_SHORT).show();
-                //권한설정 함수 실행
-                Permission();
+                Intent modify = new Intent(getActivity(), ProfileModifyActivity.class);
+                startActivityForResult(modify, requestcode);
             }
         });
         return rootview;
     }
-
-    //권한설정 _ 안드로이드스튜디오개발가이드 + 구글링
-    public void Permission(){
-        for(String permission : permission_list){
-            //권한 확인 여부
-            int access = ContextCompat.checkSelfPermission(getContext(), permission);
-            //권한 x
-            if(access == PackageManager.PERMISSION_DENIED){
-                //권한 설정 확인 창이 뜸!!
-                //requestCode는 아래의 onRequestPermissionsResult 함수 실행의 매개변수!
-                requestPermissions(permission_list,0);
-            }
-            //권한 o
-            else{
-                //해당 페이지로 이동
-                Intent intent = new Intent(getContext(), NewsGalleryActivity.class);
-                intent.putExtra("request", R.integer.newsRequestcode);
-                startActivity(intent);
-            }
-        }
-    }
-
-    //권한 요청 후 어떻게 관리할 것인지에 대한 함수 _ 개발가이드에 자세히 나와있음
     @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        if(requestCode == 0)
-        {
-            for(int i=0; i<grantResults.length; i++)
-            {
-                //사용자가 허용했을 경우!
-                if(grantResults[i]==PackageManager.PERMISSION_GRANTED){
-                    //페이지 이동
-                    Intent intent = new Intent(getContext(), NewsGalleryActivity.class);
-                    startActivity(intent);
-                }
-                //사용자가 거부했을 경우!
-                else {
-                    Toast.makeText(getContext(),"저장소 권한을 설정하세요.",Toast.LENGTH_LONG).show();
-                }
-            }
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        if(resultCode == getActivity().RESULT_OK){
+            ProfileInfo pinfo = (ProfileInfo) data.getSerializableExtra("profileinfo");
+            Log.i("username : ",pinfo.getUsername());
+            Log.i("shape : ",pinfo.getShape());
+            Log.i("meal : ",""+pinfo.getMeal());
         }
     }
 }
-
-
-
-
