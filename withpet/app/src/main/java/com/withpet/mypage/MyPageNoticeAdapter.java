24ea9e6436917e00cc5b37534a967f317779dbe3
KimJongHee <ahdwmzm@naver.com>
2020-09-15 13:16:26 +0900

    200915_12:57_종희
    프로필 수정 코드 수정

--- withpet/app/src/main/java/com/withpet/mypage/MyPageNoticeAdapter.java
+++ withpet/app/src/main/java/com/withpet/mypage/MyPageNoticeAdapter.java
@@ -38,7 +38,6 @@ public class MyPageNoticeAdapter extends RecyclerView.Adapter<MyPageNoticeAdapte
     public MyPageNoticeAdapter.MyPageNoticeViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.mypagenotice_item, parent, false);
         MyPageNoticeAdapter.MyPageNoticeViewHolder holder = new MyPageNoticeAdapter.MyPageNoticeViewHolder(view);
-        Log.i("my view count : ", ""+456456);
         return holder;
     }
 
@@ -52,19 +51,16 @@ public class MyPageNoticeAdapter extends RecyclerView.Adapter<MyPageNoticeAdapte
            ex) 화면에 출력되는 내용이 4개면 onBindViewHolder은 4번 실행되고 position은 0,1,2,3이 된다.
 
         */
-        Log.i("position:",""+position);
-        if (!draw) {
-            Log.i("url",""+myfeed.get(position).getImgUrl());
-            Log.i("size:",""+myfeed.size());
 
+        if (!draw) {
             // 게시물 내용 3개씩 끊어서 출력하기 위한  if문
             // ex) 파이어베이스에 저장된 데이터가 5개면 한 번은 3개를 그리고 한 번은 2개를 그리기 위함
             if(share < (myfeed.size()/3) ){
                 int index = 0;
                 for(; count< (position+1)*3 ; count++){
-                    Log.i("count1:",""+count);
+                    /*Log.i("count1:",""+count);
                     Log.i("iter:",""+((position+1)*3));
-                    Log.i("index:",""+index);
+                    Log.i("index:",""+index);*/
                     Glide.with(holder.itemView)
                             .load(myfeed.get(count).getImgUrl())
                             //.placeholder(R.drawable.dog)
@@ -82,7 +78,7 @@ public class MyPageNoticeAdapter extends RecyclerView.Adapter<MyPageNoticeAdapte
             }
             else{
                 for(int i = 0; i < (myfeed.size()%3) ; i++){
-                    Log.i("count2:",""+count);
+                    //Log.i("count2:",""+count);
                     Glide.with(holder.itemView)
                             .load(myfeed.get(count).getImgUrl())
                             //.placeholder(R.drawable.dog)
