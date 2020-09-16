931e499b1f87c4ab0e6c04da10a25428634d45c1
KimJongHee <ahdwmzm@naver.com>
2020-09-16 09:26:06 +0900

    200916_09:25_종희
    채팅 UI, 액티비티 추가

---
+++ withpet/app/src/main/java/com/withpet/Chat/ChatListActivity.java
@@ -0,0 +1,14 @@
+package com.withpet.Chat;
+
+import androidx.appcompat.app.AppCompatActivity;
+import com.withpet.*;
+import android.os.Bundle;
+
+public class ChatListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chat_list);
+    }
+}
