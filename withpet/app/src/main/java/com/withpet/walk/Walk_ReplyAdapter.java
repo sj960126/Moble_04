7ec2e3ebf81c795f3825c242e26c0ccc689b9a05
kwonyongjun1 <dydwns18011@gmail.com>
2020-09-23 13:23:06 +0900

    용준_게시글 완료(레이아웃만 수정하면 됨)

--- withpet/app/src/main/java/com/withpet/walk/Walk_ReplyAdapter.java
+++ withpet/app/src/main/java/com/withpet/walk/Walk_ReplyAdapter.java
@@ -1,38 +1,59 @@
 package com.withpet.walk;
 
 import android.content.Context;
+import android.content.SharedPreferences;
+import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.withpet.R;
+
 import java.util.ArrayList;
 
-public class Walk_ReplyAdapter /*extends RecyclerView.Adapter<Walk_ReplyAdapter.CustomViewholder>*/ {
+public class Walk_ReplyAdapter extends RecyclerView.Adapter<Walk_ReplyAdapter.CustomViewholder> {
 
-    /*private ArrayList<Walk_ReplyUpload> arrayList;
+    private ArrayList<Walk_ReplyUpload> arrayList;
     private Context context;
+
+
+    public Walk_ReplyAdapter(ArrayList<Walk_ReplyUpload> arrayList, Context context) {
+        this.arrayList = arrayList;
+        this.context = context;
+    }
 
     @NonNull
     @Override
     public CustomViewholder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.activity_walk_replyitem, parent, false);
+        CustomViewholder holder = new CustomViewholder(view);
+        return holder;
     }
 
     @Override
     public void onBindViewHolder(@NonNull CustomViewholder holder, int position) {
-
+        SharedPreferences preferences = context.getSharedPreferences(arrayList.get(position).getWalk_uid(), Context.MODE_PRIVATE);
+        String nickName = preferences.getString("nickName", "host");
+        holder.name_Tv.setText(nickName);
+        holder.reply_Tv.setText(arrayList.get(position).getReply());
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return (arrayList != null ? arrayList.size() : 0);
     }
 
     public class CustomViewholder extends RecyclerView.ViewHolder {
+        TextView name_Tv;
+        TextView reply_Tv;
         public CustomViewholder(@NonNull View itemView) {
             super(itemView);
+            this.name_Tv = itemView.findViewById(R.id.tv_replyname);
+            this.reply_Tv = itemView.findViewById(R.id.tv_reply);
         }
-    }*/
+    }
 }
--- withpet/app/src/main/java/com/withpet/walk/Walk_ReplyUpload.java
+++ withpet/app/src/main/java/com/withpet/walk/Walk_ReplyUpload.java
@@ -4,6 +4,27 @@ public class Walk_ReplyUpload {
     private String reply;
     private int board_nb;
     private String walk_uid;
+    private int reply_nb;
+    private String uid;
+    Walk_ReplyUpload(){
+
+    }
+    Walk_ReplyUpload(int board_nb, String reply){
+        this.board_nb = board_nb;
+        this.reply = reply;
+    }
+    Walk_ReplyUpload(int board_nb, String reply, String walk_uid){
+        this.board_nb = board_nb;
+        this.reply = reply;
+        this.walk_uid = walk_uid;
+    }
+    Walk_ReplyUpload(int board_nb, String reply, String walk_uid, int reply_nb){
+        this.board_nb = board_nb;
+        this.reply = reply;
+        this.walk_uid = walk_uid;
+        this.reply_nb = reply_nb;
+
+    }
 
     public String getWalk_uid() {
         return walk_uid;
@@ -14,14 +35,12 @@ public class Walk_ReplyUpload {
     }
 
 
-    Walk_ReplyUpload(int board_nb, String reply){
-        this.board_nb = board_nb;
-        this.reply = reply;
+    public int getReply_nb() {
+        return reply_nb;
     }
-    Walk_ReplyUpload(int board_nb, String reply, String walk_uid){
-        this.board_nb = board_nb;
-        this.reply = reply;
-        this.walk_uid = walk_uid;
+
+    public void setReply_nb(int reply_nb) {
+        this.reply_nb = reply_nb;
     }
     public String getReply() {
         return reply;
