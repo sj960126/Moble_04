e3daf817048ababcb3e86c89de78c45130d1bf1e
songhyunsu <sj96012@naver.com>
2020-10-05 10:30:09 +0900

    10/05_현수

--- withpet/app/src/main/java/com/withpet/main/loding_Activity.java
+++ withpet/app/src/main/java/com/withpet/main/loding_Activity.java
@@ -1,8 +1,10 @@
-package com.withpet.health;
+package com.withpet.main;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.Handler;
+
 import com.withpet.*;
 
 public class loding_Activity extends AppCompatActivity {
@@ -11,5 +13,15 @@ public class loding_Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_loding);
+        startLoading();
+    }
+    private void startLoading(){
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                finish();
+            }
+        },2000);
     }
 }
