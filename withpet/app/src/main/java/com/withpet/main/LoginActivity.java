db8f3a5f093a83ca49c1612f69ed6ab0162ddc9c
mangku17 <sparky17@naver.com>
2020-10-06 17:09:57 +0900

    1006_17pm 미정
    로그인처리

--- withpet/app/src/main/java/com/withpet/main/LoginActivity.java
+++ withpet/app/src/main/java/com/withpet/main/LoginActivity.java
@@ -5,7 +5,9 @@ import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
@@ -29,6 +31,7 @@ import java.util.ArrayList;
 public class LoginActivity extends AppCompatActivity {
     private EditText etEmail, etPw;
     private String strEmail, strPw;
+    private Button btnLogin, btnJoin, btnFind;
 
     private FirebaseAuth firebaseAuth;
     private FirebaseUser firebaseUser;
@@ -44,6 +47,10 @@ public class LoginActivity extends AppCompatActivity {
 
         etEmail = (EditText) findViewById(R.id.loginEt_id);
         etPw = (EditText) findViewById(R.id.loginEt_pw);
+
+        btnFind = (Button) findViewById(R.id.loginBtn_findpw);
+        btnJoin = (Button) findViewById(R.id.loginBtn_join);
+        btnLogin = (Button) findViewById(R.id.loginBtn_login);
 
         firebaseDatabase = FirebaseDatabase.getInstance();
         firebaseUser = firebaseAuth.getCurrentUser();
@@ -86,30 +93,37 @@ public class LoginActivity extends AppCompatActivity {
         loginCheck(LoginUser);
     }
 
+
+
     public void btnClick(View view){
         switch (view.getId()){
             case R.id.loginBtn_login:
                 //로그인할 이메일과 비밀번호
                 strEmail = etEmail.getText().toString().trim();
                 strPw = etPw.getText().toString().trim();
-                //파이어베이스 '인증' >> 이메일로그인
-                firebaseAuth.signInWithEmailAndPassword(strEmail, strPw).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
-                    @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
-                        // 가입  && 이메일인증 완료
-                        if(task.isSuccessful()){
-                            FirebaseUser login = firebaseAuth.getCurrentUser();
-                            if(login.isEmailVerified()){
-                                Login();
-                            }else{
-                                Toast.makeText(LoginActivity.this, "가입한 이메일로 전송한 인증메일을 수락하세요.", Toast.LENGTH_SHORT).show();
+                if(strEmail.equals("") || strPw.equals("")){
+                    Toast.makeText(this, "로그인 정보를 정확히 기입하세요.", Toast.LENGTH_SHORT).show();
+                }
+                else{
+                    //파이어베이스 '인증' >> 이메일로그인
+                    firebaseAuth.signInWithEmailAndPassword(strEmail, strPw).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            // 가입  && 이메일인증 완료
+                            if(task.isSuccessful()){
+                                FirebaseUser login = firebaseAuth.getCurrentUser();
+                                if(login.isEmailVerified()){
+                                    Login();
+                                }else{
+                                    Toast.makeText(LoginActivity.this, "가입한 이메일로 전송한 인증메일을 수락하세요.", Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                            else{
+                                Toast.makeText(LoginActivity.this, "일치하는 회원이 없습니다.", Toast.LENGTH_SHORT).show();
                             }
                         }
-                        else{
-                            Toast.makeText(LoginActivity.this, "일치하는 회원이 없습니다.", Toast.LENGTH_SHORT).show();
-                        }
-                    }
-                });
+                    });
+                }
                 break;
             case R.id.loginBtn_findpw:
                 Intent find = new Intent(LoginActivity.this, FindPwActivity.class);
